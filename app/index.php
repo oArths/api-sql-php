d8db0310109a10ec81bab5a13ff0f756c081eaef
unknown <arthurferreira1417@gmail.com>
2024-05-13 17:23:06 -0300

    Feat: refactor the request for api

--- app/index.php
+++ app/index.php
@@ -3,14 +3,16 @@
 //dependecias 
 require_once('inc/config.php');
 require_once('inc/api_functions.php');
-$variables = [
-    'Nome' => 'Arthur', 
-    'Sobrenome' => 'Ferreira', 
-];
+// $variables = [
+//     'Nome' => 'Arthur', 
+//     'Sobrenome' => 'Ferreira', 
+// ];
 
-//CHAMA A PI PASSSADNO OS DADOS COMO ENDPOINT/METOFO/VARIAVEIS
-$REPOMNSE = api_request('status', 'POST', $variables);
+//CHAMA A API PASSSADNO OS DADOS COMO ENDPOINT/METOFO/VARIAVEIS
+// $REPOMNSE = api_request('status', 'POST', $variables);
+$REPOMNSE = api_request('status', 'GET');
 
 // echo json_encode($REPOMNSE);
-echo "Resposta da API: " . json_encode($REPOMNSE);
+echo '<pre>';
+print_r($REPOMNSE);
 ?>
